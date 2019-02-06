c860b5edb8fd7b20b9f106a32673846547f9fc8b
Juni Yadi <juniyadi94@gmail.com>
2019-02-06 10:40:41 +0700

    update new methode 6 Feb 2019

--- zippyshare.sh
+++ zippyshare.sh
@@ -2,8 +2,8 @@
 # @Description: zippyshare.com file download script
 # @Author: Juni Yadi
 # @URL: https://github.com/JuniYadi/zippyshare
-# @Version: 201811261036
-# @Date: 2018-11-26
+# @Version: 201902061036
+# @Date: 2019-02-06
 # @Usage: ./zippyshare.sh url
 
 if [ -z "${1}" ]
@@ -32,15 +32,7 @@ function zippydownload()
         rm -f "${infofile}" 2> /dev/null
         curl -s -c "${cookiefile}" -o "${infofile}" -L "${url}"
 
-        filenamecheck=$( cat "${infofile}" | grep '<span id="omg" class="2" style="display:none;"></span>' )
-
-        if [ "$filenamecheck" ]; then
-            filename="$( cat "${infofile}" | grep "/d/" | cut -d'/' -f6 | cut -d'"' -f1 | grep -o "[^ ]\+\(\+[^ ]\+\)*" )"
-            echo "new" > "$fetchdlmet"
-        else
-            filename="$( cat "${infofile}" | grep "/d/" | cut -d'/' -f5 | cut -d'"' -f1 | grep -o "[^ ]\+\(\+[^ ]\+\)*" )"
-            echo "old" > "$fetchdlmet"
-        fi
+        filename="$( grep "getElementById..dlbutton...href" "${infofile}" | cut -d"/" -f5 | sed "s/\";//g" )"
     done
 
     if [ "${retry}" -ge 10 ]
@@ -60,30 +52,18 @@ function zippydownload()
 
     if [ -f "${infofile}" ]
     then
-        #Get url algorithm (new methode 26/11/2018)        
-        dlfetch=$( grep 'getElementById..dlbutton...href' "${infofile}" | grep -oE '\([0-9].*\)' > "$fetchpath" )
-
-        if [ $( cat "$fetchdlmet" ) == "new" ]; then
-            refetcha=$(sed -i "s/a()/1/g;" "$fetchpath")
-            refetchb=$(sed -i "s/b()/2/g;" "$fetchpath")
-            refetchc=$(sed -i "s/c()/3/g;" "$fetchpath")
-            refetchd=$(sed -i "s/ d / 4 /g;" "$fetchpath")
-        fi
-
-
-        dlbutton="$( cat "$fetchpath" )"
-        
+        #Get url algorithm (new methode 06/02/2019)
+        dlbutton=$( grep 'var a = ' "${infofile}" | tail -1 | cut -d" " -f8 | cut -d";" -f1 )
 
         if [ -n "${dlbutton}" ]
         then
-           algorithm="${dlbutton}"
+           algorithm=$( echo "($dlbutton * $dlbutton * $dlbutton)+3" | bc )
         else
            echo "could not get zippyshare url algorithm"
            exit 1
         fi
 
         
-        a="$( echo $(( ${algorithm} )) )"
         # Get ref, server, id
         ref="$( cat "${infofile}" | grep 'property="og:url"' | cut -d'"' -f4 | grep -o "[^ ]\+\(\+[^ ]\+\)*" )"
 
@@ -96,7 +76,7 @@ function zippydownload()
     fi
 
     # Build download url
-    dl="https://${server}/d/${id}/${a}/${filename}"
+    dl="https://${server}/d/${id}/${algorithm}/${filename}"
 
     # Set browser agent
     agent="Mozilla/5.0 (Windows NT 6.3; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.102 Safari/537.36"
